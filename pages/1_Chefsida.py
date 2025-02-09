9b25d2d10ee6956cdc8f69a28a5d419606d9e134
Johan Mäki <johan.maki@hotmail.com>
2025-02-09 09:54:27 +0100

    fix chefsida fix 4

--- pages/1_Chefsida.py
+++ pages/1_Chefsida.py
@@ -33,8 +33,8 @@ LANGUAGES = {
 def init_session():
  
     if "user_type" not in st.session_state or st.session_state.user_type != "chef":
-    st.error("Åtkomst nekad. Du är inte inloggad som chef.")
-    st.stop()
+        st.error("Åtkomst nekad. Du är inte inloggad som chef.")
+        st.stop()
 
     required_keys = ["staff", "dark_mode", "language", "user_type", "hospital"]
     for key in required_keys:
